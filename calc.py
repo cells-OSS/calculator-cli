dda79e50a642f3e82fbb9b8cd2942acd0ef4344c
cells-OSS <open-source-version@proton.me>
2025-10-18 23:39:34 +0300

    setting up custom locations

--- calc.py
+++ calc.py
@@ -44,23 +44,32 @@ def download_latest_script():
     input("Press Enter to exit...")
     exit()
 
+if os.name == "nt":
+    config_dir = os.path.join(os.getenv("APPDATA"), "pyculator")
+else:
+    config_dir = os.path.expanduser("~/.config/pyculator")
 
-if os.path.exists("welcome_message.conf"):
-    with open("welcome_message.conf", "rb") as configFile:
+welcomeMessage_config_path = os.path.join(config_dir, "welcome_message.conf")
+figlet_config_path =  os.path.join(config_dir, "figlet.conf")
+auto_update_config_path = os.path.join(config_dir, "auto_update.conf")
+
+
+if os.path.exists(welcomeMessage_config_path):
+    with open(welcomeMessage_config_path, "rb") as configFile:
         welcomeMessage = configFile.read().decode()
 else:
     welcomeMessage = """
     ===============WELCOME===============
     """
 
-if os.path.exists("figlet.conf"):
-    with open("figlet.conf", "rb") as figlet_configFile:
+if os.path.exists(figlet_config_path):
+    with open(figlet_config_path, "rb") as figlet_configFile:
         figlet_config = figlet_configFile.read().decode()
         if figlet_config == "True":
             welcomeMessage = pyfiglet.figlet_format(welcomeMessage)
 
-if os.path.exists("auto_update.conf"):
-    with open("auto_update.conf", "rb") as auto_update_configFile:
+if os.path.exists(auto_update_config_path):
+    with open(auto_update_config_path, "rb") as auto_update_configFile:
         auto_update_config = auto_update_configFile.read().decode()
         if auto_update_config == "True":
             if is_update_available(__version__):
